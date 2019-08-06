e4f35c523578c5e9ac59e1a4737e1c7676524cec
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-06 16:13:58 -0400

    changed code

--- js/index.js
+++ js/index.js
@@ -11,24 +11,6 @@ navText.forEach(element => {
   });
 });
 
-//WHEN THE WHEEL IS SCROLLED IN NIGHTTIME UI THE LOGO FLASHES THROUGH THE COLORS OF THE RAINBOW
-document.addEventListener("scroll", () => {
-  if (h1.style.color === "white") {
-    h1.style.color = "red";
-  } else if (h1.style.color === "red") {
-    h1.style.color = "orange";
-  } else if (h1.style.color === "orange") {
-    h1.style.color = "yellow";
-  } else if (h1.style.color === "yellow") {
-    h1.style.color = "green";
-  } else if (h1.style.color === "green") {
-    h1.style.color = "blue";
-  } else if (h1.style.color === "blue") {
-    h1.style.color = "purple";
-  } else if (h1.style.color === "purple") {
-    h1.style.color = "red";
-  }
-});
 
 //WHEN SIGN ME UP BUTTONS ARE CLICKED THEY WILL BECOME DARKER BLUE, IF CLICKED AGAIN THEY WILL GO BACK TO ORIGINAL COLOR
 //STOPPED PROPAGATION, CLICK DOES NOT TRIGGER THE CLICK EVENT OF THE BODY
